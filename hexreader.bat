95a4a9aca8c7551598706b19cdaec11819134289
Gerasim Gerasimov <dialix@yandex.ru>
2023-01-16 14:34:25 +0700

    Узнал как переменные в bat-файлах создавать и из использовать ;-) Пути заданы переменными HexReader - путь до исполняемого файла HexReader Config - путь до config.json

--- hexreader.bat
+++ hexreader.bat
@@ -1,1 +1,3 @@
-node d:\Project\Projects\STM32F4-BOOTLOADER\Info\HexReader\dist\index.js d:/Project/EFi_Tomsk/FirmWare/OLED_BAREMETALL_V3/config.json
+set HexReader=d:\Project\Projects\STM32F4-BOOTLOADER\Info\HexReader\dist\index.js
+set Config=d:/Project/EFi_Tomsk/FirmWare/OLED_BAREMETALL_V3/config.json
+node %HexReader% %Config%
